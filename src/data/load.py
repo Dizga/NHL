f0eb47fcc098a6be5d951f197bad6f6d77c802f8
mansooraliamiri <mansooraliamiri@yahoo.com>
2023-10-14 16:36:51 -0400

    Remodifier à nouveau la création du dossier

--- src/data/load.py
+++ src/data/load.py
@@ -46,6 +46,7 @@ def load_data(year:int, filename: str = "", samples: bool = False) -> object:
   """
     # Définier de chemain
   dir_data = 'data'
+  
   # Créer de dossier DATA 
   try:
     if not os.path.isdir(dir_data):
@@ -53,7 +54,7 @@ def load_data(year:int, filename: str = "", samples: bool = False) -> object:
       print("Le dessier '% s' a crée" % dir_data) 
   except FileExistsError as e:
       print("Erreur dans le temps créer de dessier '% s' avec : "+e % dir_data) 
-      
+
   season_fullname = utils.season_full_name(year)
 
   default_filename = f'data/{season_fullname}.pkl'
