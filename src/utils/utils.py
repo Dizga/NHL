b0e775cb3b85c5f0d8367d4f35059bc3a7e1788a
ImenJaoua <jaouaimen11@gmail.com>
2023-10-16 10:22:22 -0400

    update utils.py

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -1,5 +1,7 @@
 import numpy as np
 
+def season_full_name(season: int or str):
+  return season + str(int(season)+1) if type(season) == str else f'{season}{season+1}'
 
 def normalize(arr, min_value=0, max_value=1):
     min_x = np.min(arr)
