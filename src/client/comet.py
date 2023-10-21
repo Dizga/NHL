ad2b4065b3cf2c390cfa1864991292a8d10dffcb
Dizga <alexandre.ganito@gmail.com>
2023-10-21 13:27:18 -0400

    Add workspace attribute to comet.py

--- src/client/comet.py
+++ src/client/comet.py
@@ -4,9 +4,11 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
-def start_experiment(project_name = ""):
+def start_experiment(workspace = "", project_name = ""):
     project_name = project_name or os.getenv('PROJECT_NAME')
+    workspace = workspace or os.getenv('WORKSPACE')
     return Experiment(
         api_key=os.getenv('COMET_API_KEY'),
         project_name=project_name,
+        workspace=workspace
     )
