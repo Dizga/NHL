dc28aae52457221355f5a675990b897bac378751
mansooraliamiri <mansooraliamiri@yahoo.com>
2023-11-06 13:26:41 -0500

    Détails des pénalités

--- src/features/jeu_puissance.py
+++ src/features/jeu_puissance.py
@@ -12,3 +12,28 @@ def get_seconds(time_str:str)-> int:
 
 
 
+# Détails des pénalités
+def detail_penality(season_data):
+    penalty_plays = season_data['regulars'][0]['liveData']['plays']['penaltyPlays']
+    #print('penalty_plays: ',penalty_plays)
+    all_penalties = []
+
+    for play in penalty_plays:
+        penalty_info = season_data['regulars'][0]['liveData']['plays']['allPlays'][play]
+        # Trouver des données  
+        penalty_team = penalty_info['team']['triCode']
+        penalty_time = penalty_info['about']['periodTime']
+        penalty_period = penalty_info['about']['period']
+        penalty_description = penalty_info['result']['description']
+        
+        # Ajouter dans List pour retourner
+        penalty_details = {
+            'penalty_period':penalty_period,
+            'penalty_team':penalty_team,
+            'penalty_time':get_seconds(penalty_time),
+            'penalty_description':penalty_description
+            }
+        all_penalties.append(penalty_details)
+        #print(all_penalties)
+        return all_penalties
+        
